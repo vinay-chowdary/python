e6e401c1206a8980413ebb2adee818e4f259d1f0
vinay-chowdary <vinaychowdary.dev@gmail.com>
2020-08-21 19:00:26 +0530

    add test file

---
+++ toImportInHelloWorldScript.py
@@ -0,0 +1,17 @@
+a = 'dead'
+b = 'parrot'
+c = 'sketch'
+print(a, b, c)
+
+
+# __name__ is inbuilt variable which
+# holds the mode of interpreting(as main or as any other file is importing this file)
+# if it is running as main it will execute the following if statement
+if __name__ == "__main__":
+    print("if running as main")
+
+
+# def main():
+#     print("hello")
+
+# main()
