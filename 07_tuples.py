d06c6621bdff66d5ce9106bc4a55f8ac810c864e
vinay-chowdary <vinaychowdary.dev@gmail.com>
2020-08-29 21:19:58 +0530

    added more info on tuples

--- 07_tuples.py
+++ 07_tuples.py
@@ -42,3 +42,61 @@ print(lst)
 
 # difference between class.sort() and sorted(item) is sort() rearranges the elements by modifying original list.
 # where as sorted() gives an another list instead of modifying the original.
+
+
+t = 10, 20, 30
+t += (4,)
+# t[2]=10   #cannot modify tuples
+
+# not an inplace change
+
+
+def update(t):
+    t = t*2
+    print(t)
+
+
+update(t)
+print(t)
+
+# inplace change will not work since tuples are immutable
+
+
+def updateInPlace(t):
+    t *= 2
+    print(t)
+
+
+updateInPlace(t)
+print(t)
+
+# concatenating tuples
+t1 = "1", "2"
+t += t1
+print(t)
+
+t2 = t, t1
+print(t2)
+
+# slicing
+
+print(t[2:4])
+print(t[1::3])
+print(t[-1::-2])
+
+t3 = tuple("vinay")
+print(t3)
+
+
+tup = tuple(range(1, 15))
+print(max(tup), min(tup), len(tup))
+tup = tup+([100, 99, 98], ("v", "i"))
+print(tup)
+# tup[14] = tup[14] + [10, 20, 30] item assignment is not allowed
+tup[14][2] = "added extra"
+print(tup)
+
+
+print(tup.count(10))
+print(tup.index(14))
+print(sum(tup[0:14]))
