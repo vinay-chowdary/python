399b7d1dc83956690cdc4adeef5ca3bc84eb9162
vinay-chowdary <vinaychowdary.dev@gmail.com>
2020-08-29 19:35:27 +0530

    numbers and their operations and functions

---
+++ 03_Numbers.py
@@ -0,0 +1,11 @@
+import math
+import random
+print(2+1)
+print(2-1)
+print(int(5//3.2))
+print(round(2/3.3, 3))
+print(float(2*3))
+# numbers donot have len (because numbers are not iterable ) so converted into str
+print(len(str(2**48)))
+print(math.factorial(4), math.pi, math.sqrt(16), math.sin(9))
+print(random.random(), random.choice(range(1, 20)), random.randint(1, 10))
