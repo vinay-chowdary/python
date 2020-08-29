0dc6b63f6d8660ce56323ad5fd643d33ff39dc5c
vinay-chowdary <vinaychowdary.dev@gmail.com>
2020-08-29 19:34:45 +0530

    datatypes

---
+++ 02_dataTypes_Operators.py
@@ -0,0 +1,10 @@
+# Numbers : 1234 , 3.1415 , 3+4j , 0b111 , Decimal() , Fraction()
+# Strings : 'spam' , "Bob's" , b'a\x01c' , u'sp\xc4m'
+# Lists   : [1, [2, 'three'], 4.5] , list(range(10))
+# Dictionaries  :   {'food': 'spam', 'taste': 'yum'} , dict(hours=10)
+# Tuples  : (1, 'spam', 4, 'U') , tuple('spam') , namedtuple
+# Files   : open('eggs.txt') , open(r'C:\ham.bin', 'wb')
+# Sets    : set('abc') , {'a', 'b', 'c'}
+# Other core types  : Booleans, types, None
+# Program unit types Functions, modules, classes (Part IV, Part V, Part VI)
+# Implementation-related types Compiled code, stack tracebacks (Part IV, Part VII)
